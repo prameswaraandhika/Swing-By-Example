cc8f4e779c709c249d7da1ca7d7f39592a4dcdc6
Andhikap <prameswaara@gmail.com>
2023-07-04 13:12:20 +0700

    report file updated v1

--- src/main/java/belajar_layouting/GridLayoutExample.java
+++ src/main/java/belajar_layouting/GridLayoutExample.java
@@ -1,10 +1,11 @@
-package com.mycompany.belajarlayouting;
+package belajar_layouting;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -22,15 +23,25 @@ public class GridLayoutExample extends JFrame {
     public GridLayoutExample() throws HeadlessException {
         setSize(new Dimension(500, 400));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2, 2));
+        setLayout(new GridLayout(1, 2));
 //        1 ROW 2 COL
-        add(new JButton("BUTTON 1"));
-        add(new JButton("BUTTON 1A"));
 
-//        1 ROW 2 COL
-        add(new JButton("BUTTON 2"));
-        add(new JButton("BUTTON 2A"));
+        add(new JLabel(new ImageIcon(GridLayoutExample.class.getResource("resources/ronaldo.jpg"))));
+        add(new JLabel(new ImageIcon(GridLayoutExample.class.getResource("/resources/messi.jpg"))));
+
+///
+////        1 ROW 2 COL
+//        add(new JButton("BUTTON 2"));
+//        add(new JButton("BUTTON 2A"));
         setVisible(true);
+
+//        Behaviour
+//        Objek akan diatur dengan kiri ke kanan lebih dulu lalu diikuti dengan atas kebawah
+//        Jika kita tambahkan 1 objek lagi maka dia akan menjadi 2 row 3 col
+//        add(new JButton("BUTTON 3"));
+//        Jika kita tambahkan 2 objek lagi maka dia akan menjadi 3 row 3 col
+//        add(new JButton("BUTTON 3"));
+//        add(new JButton("BUTTON 3"));
     }
 
     public static void main(String[] args) {
